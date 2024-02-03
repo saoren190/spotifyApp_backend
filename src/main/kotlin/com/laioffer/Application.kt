e6526af42130a62b90e7fd071b174bb417f860f0
wangq <wangquanze922@gmail.com>
2024-02-03 10:36:23 -0600

    Update Application.kt

--- src/main/kotlin/com/laioffer/Application.kt
+++ src/main/kotlin/com/laioffer/Application.kt
@@ -1,20 +1,21 @@
 package com.laioffer
 
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
-import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.http.content.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.Json
 
 @Serializable
-data class Song (
-    val name : String,
+data class Song(
+    val name: String,
     val lyric: String,
     val src: String,
     val length: String
@@ -29,9 +30,12 @@ data class Playlist(
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
+
 }
 
+// extension
 fun Application.module() {
+    // json <-> kotlin
     install(ContentNegotiation) {
         json(Json {
             prettyPrint = true
@@ -43,10 +47,24 @@ fun Application.module() {
             call.respondText("Hello World!")
         }
 
-        get ("/feed"){
-            val jsonString = this :: class.java.classLoader.getResource("feed.json").readText()
+        get("/feed") {
+            val jsonString = this::class.java.classLoader.getResource("feed.json").readText()
             val json = Json.parseToJsonElement(jsonString)
             call.respondText(json.toString(), ContentType.Application.Json)
+        }
+
+        get("/playlists") {
+            val jsonString = this::class.java.classLoader.getResource("playlists.json").readText()
+            val json = Json.parseToJsonElement(jsonString)
+            call.respondText(json.toString(), ContentType.Application.Json)
+        }
+
+        get("/playlist/{id}") {
+            val jsonString = this::class.java.classLoader.getResource("playlists.json").readText()
+            val playlists = Json.decodeFromString(ListSerializer(Playlist.serializer()), jsonString)
+            val id: String? = call.parameters["id"]
+            val playlist: Playlist? =  playlists.firstOrNull { item -> item.id.toString() == id  }
+            call.respondNullable(playlist)
         }
 
         static("/") {
@@ -57,3 +75,4 @@ fun Application.module() {
         }
     }
 }
+
